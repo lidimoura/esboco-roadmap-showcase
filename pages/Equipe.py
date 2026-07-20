9d0a2d54eb460cef3f64dfe8fd7d1f7d4ed7cbfb
Lídi Moura <lidimfc@gmail.com>
2026-07-20 10:54:49 -0400

    Update Equipe.py

--- pages/Equipe.py
+++ pages/Equipe.py
@@ -52,7 +52,7 @@ def app():
             "specialty": t("specialty_mayara"),
             "linkedin": None,
             "github": None,
-            "avatar": "https://avatars.githubusercontent.com/u/298712900?v=4"
+            "avatar": "https://avatars.githubusercontent.com/u/000000900?v=4"
         },
         {
             "name": "Alex Furukawa",
