59119fb9e756862fb6fd425f07b116f47cf3353c
Lídi Moura <lidimfc@gmail.com>
2026-07-20 10:52:46 -0400

    fix: remove syntax error (double colons) and __main__ guard from Equipe.py

--- pages/Equipe.py
+++ pages/Equipe.py
@@ -52,7 +52,7 @@ def app():
             "specialty": t("specialty_mayara"),
             "linkedin": None,
             "github": None,
-            "avatar": : "https://avatars.githubusercontent.com/u/298712900?v=4"
+            "avatar": "https://avatars.githubusercontent.com/u/298712900?v=4"
         },
         {
             "name": "Alex Furukawa",
@@ -76,7 +76,7 @@ def app():
             "specialty": t("specialty_dryelli"),
             "linkedin": None,
             "github": None,
-            "avatar": : "https://avatars.githubusercontent.com/u/298712900?v=4"
+            "avatar": "https://avatars.githubusercontent.com/u/298712900?v=4"
         },
     ]
 
@@ -143,6 +143,3 @@ def app():
     ### {t("collaboration_title")}
     {t("collaboration_text")}
     """)
-
-if __name__ == "__main__":
-    app()
